c1f839c039079f6b93d83f41bb9f416697d62c87
Colleen Murray <colleenkhm@gmail.com>
2022-07-06 19:38:09 -0500

    User and Event models

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,20 +1,31 @@
 const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   description: {
     type: String,
     required: true,
   },
-  // saved book id from GoogleBooks
-  bookId: {
-    type: String,
+  game: [
+    {
+      type: String,
+    }
+  ],
+  attendees: [
+    {
+      type: String,
+    }
+  ],
+  startDate: {
+    type: Date,
     required: true,
+  },
+  endDate: {
+    type: Date,
   },
   image: {
     type: String,
@@ -22,10 +33,6 @@ const bookSchema = new Schema({
   link: {
     type: String,
   },
-  title: {
-    type: String,
-    required: true,
-  },
 });
 
-module.exports = bookSchema;
+module.exports = eventSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Book.js
-const bookSchema = require('./Book');
+// import schema from User.js
+// const eventSchema = require('./Event');
 
 const userSchema = new Schema(
   {
@@ -21,8 +21,22 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+    },
+    role: [
+      {
+      type: String,
+      required: true,
+      }
+    ],
+    games: [
+      {
+        type: String,
+      }
+    ]
     // set savedBooks to be an array of data that adheres to the bookSchema
-    savedBooks: [bookSchema],
+    // savedBooks: [bookSchema],
   },
   // set this to use virtual below
   {
@@ -47,10 +61,10 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
+// // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+// userSchema.virtual('bookCount').get(function () {
+//   return this.savedBooks.length;
+// });
 
 const User = model('User', userSchema);
 
